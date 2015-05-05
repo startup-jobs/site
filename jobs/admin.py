d7c4703c4aa63762c494be876b3ff85d3fca2324
Robert George <robert@justeat.in>
2015-05-05 22:31:56 +0530

    Refs #2 	sorting, filtering added  On branch admin-backend 	modified:   jobs/admin.py

--- jobs/admin.py
+++ jobs/admin.py
@@ -1,8 +1,15 @@
 from django.contrib import admin
 from models import JobProfile, City, Organization, JobType, JobCategory
 
+
+class JobAdmin(admin.ModelAdmin):
+	list_display = ('title', 'location', 'job_type', 'job_category','created')
+	search_fields = ('location__name', 'title')
+	list_filter = ('created', )
+	ordering = ('-created', )
+
 # Register your models here.
-admin.site.register(JobProfile)
+admin.site.register(JobProfile, JobAdmin)
 admin.site.register(City)
 admin.site.register(Organization)
 admin.site.register(JobType)
